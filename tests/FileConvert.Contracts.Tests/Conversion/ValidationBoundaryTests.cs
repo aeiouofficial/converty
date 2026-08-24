1269dcadc95a343428b00753dd5d8aac071333ec
aeiou <bigbred1992@googlemail.com>
2026-08-24 22:59:09 +0200

    test: align boundary tests with current contracts

--- tests/FileConvert.Contracts.Tests/Conversion/ValidationBoundaryTests.cs
+++ tests/FileConvert.Contracts.Tests/Conversion/ValidationBoundaryTests.cs
@@ -7,28 +7,28 @@ namespace FileConvert.Contracts.Tests.Conversion;
 public sealed class ValidationBoundaryTests
 {
     [Fact]
-    public void ConversionRequest_rejects_path_longer_than_wire_limit()
+    public void ConversionRequestRejectsPathLongerThanWireLimit()
     {
         var path = new string('a', ConversionRequest.MaximumPathLength + 1);
         Assert.Throws<ArgumentException>(() => new ConversionRequest(SchemaVersions.Current, Guid.NewGuid(), ConversionAction.ConvertUsingDefault, [path], null, null));
     }
 
     [Fact]
-    public void ConversionPreset_rejects_too_many_options()
+    public void ConversionPresetRejectsTooManyOptions()
     {
         var options = Enumerable.Range(0, ConversionPreset.MaximumOptions + 1).ToDictionary(index => $"key{index}", _ => "value", StringComparer.Ordinal);
         Assert.Throws<ArgumentException>(() => new ConversionPreset(SchemaVersions.Current, PresetId.Parse("audio.mp3.high"), "High", FileFamilyId.Parse("audio"), FormatId.Parse("audio.mp3"), null, options));
     }
 
     [Fact]
-    public void FormatDescriptor_rejects_too_many_extensions()
+    public void FormatDescriptorRejectsTooManyExtensions()
     {
         var extensions = Enumerable.Range(0, FormatDescriptor.MaximumExtensions + 1).Select(index => $".x{index}").ToArray();
-        Assert.Throws<ArgumentException>(() => new FormatDescriptor(SchemaVersions.Current, FormatId.Parse("image.test"), FileFamilyId.Parse("image"), "Test", extensions[0], extensions));
+        Assert.Throws<ArgumentException>(() => new FormatDescriptor(FormatId.Parse("image.test"), FileFamilyId.Parse("image"), "Test", extensions[0], extensions, SchemaVersions.Current));
     }
 
     [Fact]
-    public void Root_descriptors_expose_current_schema_version()
+    public void RootDescriptorsExposeCurrentSchemaVersion()
     {
         var format = new FormatDescriptor(FormatId.Parse("audio.wav"), FileFamilyId.Parse("audio"), "WAV", ".wav", [".wav"]);
         var capability = new CapabilityDescriptor(ProviderId.Parse("fake.audio"), FormatId.Parse("audio.wav"), FormatId.Parse("audio.mp3"), ConversionMode.Transcode, 100);
@@ -37,7 +37,7 @@ public sealed class ValidationBoundaryTests
     }
 
     [Fact]
-    public void Request_and_probe_paths_reject_embedded_nul()
+    public void RequestAndProbePathsRejectEmbeddedNul()
     {
         var badPath = "chapter\0.wav";
         Assert.Throws<ArgumentException>(() => new ConversionRequest(SchemaVersions.Current, Guid.NewGuid(), ConversionAction.ConvertUsingDefault, [badPath], null, null));
