5a6cc5614c29219450d4ac8684f7f860817078b2
aeiou <bigbred1992@googlemail.com>
2026-08-25 03:11:49 +0200

    feat: add Host job admission result

---
+++ src/Converty.Host/Jobs/JobAdmissionResult.cs
@@ -0,0 +1,17 @@
+namespace Converty.Host.Jobs;
+
+public enum JobAdmissionRejection
+{
+    None = 0,
+    DuplicateRequest = 1,
+    QueueFull = 2,
+}
+
+public readonly record struct JobAdmissionResult(Guid JobId, JobAdmissionRejection Rejection)
+{
+    public bool Accepted => Rejection == JobAdmissionRejection.None;
+
+    public static JobAdmissionResult Accept(Guid jobId) => new(jobId, JobAdmissionRejection.None);
+
+    public static JobAdmissionResult Reject(JobAdmissionRejection rejection) => new(Guid.Empty, rejection);
+}
